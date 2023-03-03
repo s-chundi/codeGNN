ead234e6bc6ed4d52dda912528c773c7bb2b0b90
Suhas Chundi <suhaschundi@DN0a2326d2.SUNet>
2023-03-03 11:25:41 -0800

    Extract AST, Add simple files for viewing ast

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # codeGNN
+
+Our CS 224W final project
+Collaborators: Suhas Chundi, Patrick Donahue, Abhay Singhal
---
+++ cppast.py
@@ -0,0 +1,72 @@
+import clang.cindex
+import networkx as nx
+from networkx.drawing.nx_agraph import to_agraph
+import graphviz
+import tempfile
+import matplotlib.pyplot as plt
+
+# create an index
+clang.cindex.Config.set_library_file('/Library/Developer/CommandLineTools/usr/lib/libclang.dylib')
+index = clang.cindex.Index.create()
+
+input_filename = 'simple.txt'
+output_filename = 'output.cpp'
+first = True
+
+with open(input_filename, 'r') as infile, open(output_filename, 'w') as outfile:
+    for line in infile:
+        outfile.write(line)
+
+root = index.parse(output_filename)
+
+# A dict to show label attributes. 
+label_dict = {}
+
+def traverse_ast(node, parent=None, graph=None, first=False):
+    if node.kind.name == "INTEGER_LITERAL":
+        print(dir(node))
+        print(dir(node.kind))
+        print(node.get_arguments)
+
+
+    if graph is None:
+        graph = nx.DiGraph()
+    if node.kind.is_unexposed():
+        # Unexposed nodes are compiler specific and should be skipped over
+        for child in node.get_children():
+            traverse_ast(child, parent, graph)
+    else:
+        # Add the current node to the graph
+        node_id = str(node.hash)
+        node_label = node.kind.name
+        # maybe need to make sure node label is 7 chars long
+        if str(node_label)[-7:] == "LITERAL":
+            # Differentiating string literals and other numeric literals, who knows if this is a good idea or not
+            if node_label[:6] == "STRING":
+                # This line is here only for visualization purposes. If we are running some algo on this graph then don't do this
+                node_label += ' ' + str(node.spelling)
+
+                graph.add_node(node_id, label=node_label, string_val = node.spelling)
+            else:
+                graph.add_node(node_id, label=node_label, literal_val = node.spelling)
+            
+        else:
+            graph.add_node(node_id, label=node_label)
+
+        label_dict[node_id] = node_label
+        # Add an edge from the current node to its parent
+        if parent is not None:
+            graph.add_edge(node_id, parent)
+        # Recursively traverse the children of the current node
+        for child in node.get_children():
+            traverse_ast(child, node_id, graph)
+    return graph
+
+# Traverse the AST and construct the graph
+ast_graph = traverse_ast(root.cursor, first=True)
+nx.draw(ast_graph, labels=label_dict, with_labels = True)
+plt.show()
+print(f"The graph has {len(ast_graph.nodes)} nodes")
+print(f"The graph has {len(ast_graph.edges)} edges")
+# agraph = to_agraph(ast_graph)
+# agraph.draw('sample_ast.png', prog='sfdp')

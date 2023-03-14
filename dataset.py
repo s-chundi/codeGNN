f99b8dc27c69af6d69a97bc3c2ab55c743cd1903
Abhay Singhal <abhayaaditsinghal@gmail.com>
2023-03-14 01:49:06 -0700

    converted json to pyg data list

--- dataset.py
+++ dataset.py
@@ -2,6 +2,8 @@ import cppast
 import nx_pyg
 
 import os
+import json
+import torch
 
 def txt_to_pyg_data(input_filename, task_label, index):
     ast_graph = cppast.txt_to_nx_graph(input_filename)
@@ -18,10 +20,23 @@ def get_file_paths(data_dir):
             file_paths.append(filepath)
     return file_paths
 
+
+def json_to_pyg_dataset(input_filename, output_filename):
+    pyg_dataset = []
+    with open(input_filename, 'r') as f:
+        for line in f:
+            item = json.loads(line)
+            tmp_filename = 'tmp.txt'
+            with open(tmp_filename, 'w') as f:
+                f.write(item['code'])
+            pyg_data = txt_to_pyg_data(tmp_filename, item['label'], item['index'])
+            pyg_dataset.append(pyg_data)
+    torch.save(pyg_dataset, output_filename)
+    print(len(pyg_dataset))
+    return pyg_dataset
+
 if __name__ == '__main__':
-    input_filename = 'simple.txt'
-    task_label = 1
-    index = 0
-    pyg_data = txt_to_pyg_data(input_filename, 1, 0)
-    print(pyg_data)
+    pyg_data = json_to_pyg_dataset('Clone-detection-POJ-104/dataset/train.jsonl', 'data/poj-104/train.pt')
+    pyg_data = json_to_pyg_dataset('Clone-detection-POJ-104/dataset/valid.jsonl', 'data/poj-104/valid.pt')
+    pyg_data = json_to_pyg_dataset('Clone-detection-POJ-104/dataset/test.jsonl', 'data/poj-104/test.pt')
 
--- output.cpp
+++ output.cpp
@@ -1,21 +1,26 @@
-// This is a sample c++ input program 
-#include <iostream>
-#include <string>
-using namespace std;
+/*
+ * hanshudigui2.cpp
+ *
+ *  Created on: 2010-11-24
+ *      Author: hanzhe
+ */
 
-int main(){
-    int n;
-    cin >> n;
-    bool longword;
-    string w;
-    for (int i = 0; i < n; i++){
-        cin >> w;
-        longword = w.length() > 10;
-        if(longword){
-            cout << w[0] << w.length() - 2 << w[w.length() - 1] << endl;
-        } else {
-            cout << w << endl;
-        }
-    }
-    return 0;
+
+int f(int x,int y){       //??????????
+	int g=0;              //g=1?????????
+	for(int i=0;;i++){    //??????(x/2^n)=(y/2^m)
+		for(int j=0;(y/pow(2.0,j))>=1;j++)
+			if((int)(x/pow(2.0,i))==(int)(y/pow(2.0,j))){
+				return x/pow(2.0,i);
+				g=1;
+				break;
+			}
+	if(g==1)break;
+	}
 }
+int main(){
+	int n1,n2;
+	cin>>n1>>n2;
+	cout<<f(n1,n2);
+	return 0;
+}                        //????

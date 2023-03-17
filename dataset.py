ad27bf4c0a43de7f1ff123641c2d970069e0efc7
Abhay Singhal <20944075+sAbhay@users.noreply.github.com>
2023-03-16 21:08:08 -0700

    regenerated data as separate data files

--- dataset.py
+++ dataset.py
@@ -21,8 +21,9 @@ def get_file_paths(data_dir):
     return file_paths
 
 
-def json_to_pyg_dataset(input_filename, output_filename):
-    pyg_dataset = []
+def json_to_pyg_dataset(input_filename, output_filedir):
+    # pyg_dataset = []
+    i = 0
     with open(input_filename, 'r') as f:
         for line in f:
             item = json.loads(line)
@@ -30,13 +31,12 @@ def json_to_pyg_dataset(input_filename, output_filename):
             with open(tmp_filename, 'w') as f:
                 f.write(item['code'])
             pyg_data = txt_to_pyg_data(tmp_filename, int(item['label']), int(item['index']))
-            pyg_dataset.append(pyg_data)
-    torch.save(pyg_dataset, output_filename)
-    print(len(pyg_dataset))
-    return pyg_dataset
+            torch.save(pyg_data, f'{output_filedir}/{i}.pt')
+            i += 1
+    print(i)
 
 if __name__ == '__main__':
-    pyg_data = json_to_pyg_dataset('Clone-detection-POJ-104/dataset/train.jsonl', 'data/poj-104/train.pt')
-    pyg_data = json_to_pyg_dataset('Clone-detection-POJ-104/dataset/valid.jsonl', 'data/poj-104/valid.pt')
-    pyg_data = json_to_pyg_dataset('Clone-detection-POJ-104/dataset/test.jsonl', 'data/poj-104/test.pt')
+    json_to_pyg_dataset('Clone-detection-POJ-104/dataset/train.jsonl', 'data/poj-104/train')
+    json_to_pyg_dataset('Clone-detection-POJ-104/dataset/valid.jsonl', 'data/poj-104/valid')
+    json_to_pyg_dataset('Clone-detection-POJ-104/dataset/test.jsonl', 'data/poj-104/test')
 
